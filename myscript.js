f39a963074c72e4e2bbfa9ef3f7a6344820c874d
Yoshiyuki Kawashima <ykawashi7@gmail.com>
2015-07-23 12:10:19 +0900

    フィルターを使ってみよう
    $scope が使えるか試したらできた。
    スコープを限定したい場合は変更前のようにインスタンスに紐付ける感じか。

--- myscript.js
+++ myscript.js
@@ -1,9 +1,9 @@
 angular.module('myApp', [])
-    .controller('myAppController', function() {
-        var myAppCtrl = this;
-        myAppCtrl.users = [
+    .controller('myAppController', function($scope) {
+        $scope.users = [
             {"name":"taguchi", "score":52.22},
             {"name":"tanaka", "score":38.22},
             {"name":"yamada", "score":11.11}
         ];
+        $scope.today = new Date();
     });
