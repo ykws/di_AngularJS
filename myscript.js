766bbbe5c6bbf07380baab1c91ee59893f88c747
Yoshiyuki Kawashima <ykawashi7@gmail.com>
2015-07-23 14:38:47 +0900

    フォームの基本を学ぼう

--- myscript.js
+++ myscript.js
@@ -1,18 +1,6 @@
 angular.module('myApp', [])
     .controller('myAppController', function($scope) {
-        $scope.users = [
-            {"name":"taguchi", "score":52.22},
-            {"name":"tanaka", "score":38.22},
-            {"name":"yamada", "score":11.11},
-            {"name":"hayashi", "score":5.25},
-            {"name":"nanahashi", "score":82.4},
-            {"name":"yasuda", "score":55.21},
-            {"name":"minami", "score":32.8},
-            {"name":"yanagi", "score":72.2}
-        ];
-    })
-    .controller('userItemController', function($scope) {
-        $scope.increment = function() {
-            $scope.user.score++;
+        $scope.addUser = function() {
+          
         }
     });
